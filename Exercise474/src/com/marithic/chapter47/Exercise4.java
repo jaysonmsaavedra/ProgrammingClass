60646ae5e5dba4e98aea19c02b43474dc4e48b9f
Jayson Saavedra <jaysonmsaavedra@gmail.com>
2015-11-09 21:44:44 -0800

    Fixed for loop with new int[]

--- Exercise474/src/com/marithic/chapter47/Exercise4.java
+++ Exercise474/src/com/marithic/chapter47/Exercise4.java
@@ -1,22 +1,21 @@
 package com.marithic.chapter47;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Exercise4 {
 
 	static int[] data = {1,2,3,4,5,6,7,6,8,9,10,11,12,13,14};
 	
 	public static void main(String[] args) {
 		
-		System.out.println(reverseAndRewrite());
+		for (int datum: reverseAndRewrite()) {
+			System.out.print(datum + ",");
+		}
 	}
 
-	public static List<Integer> reverseAndRewrite() {
-		List<Integer> reverseData = new ArrayList<Integer>();
+	public static int[] reverseAndRewrite() {
+		int[] reverseData = new int[data.length];
 		
-		for (int counter=data.length - 1; counter > 0; counter--) {
-			reverseData.add(counter);
+		for (int counter=data.length - 1; counter >= 0; counter--) {
+			reverseData[(reverseData.length - 1) - counter] = data[counter];
 		}
 		
 		return reverseData;
