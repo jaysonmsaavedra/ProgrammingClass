56245cc15b137751277fef0fd9227dca6477983a
jaysonmsaavedra <jaysonmsaavedra@gmail.com>
2015-11-17 08:59:17 -0700

    Finished chapter 48, 49C1, and began 49C2

---
+++ Exercise49C2/src/com/marithic/chapter49C/Exercise2.java
@@ -0,0 +1,30 @@
+package com.marithic.chapter49C;
+
+public class Exercise2 {
+	
+	public static void main ( String[] args ) 
+	  {
+	    int[][] data = { {3, 2, 5},
+	                     {1, 4, 4, 8, 13},
+	                     {9, 1, 0, 2},
+	                     {0, 2, 6, 3, -1, -8} };
+	    
+	    // declare the sum
+	    int sumOfRow = 0;
+	    int sum = 0;
+	    // compute the sums for each row
+	    for ( int row=0; row < data.length; row++) {
+	      // initialize the sum
+	      sumOfRow += data[row][0];
+	      
+	      // compute the sum for this row
+	      for ( int col=0; col < data[col].length; col++) {
+	         
+	      }
+	      
+	      // write the sum for this row
+	      System.out.println(  );
+	    }
+	      
+	  }
+}
